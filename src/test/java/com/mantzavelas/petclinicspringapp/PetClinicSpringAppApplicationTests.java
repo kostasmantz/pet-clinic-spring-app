3c75f0c0edf6939d32ccb7298dfe2ce24f7e241e
Kostas Mantzavelas <kostiksm@gmail.com>
2020-05-03 22:19:10 +0300

    Initial Project Commit

---
+++ src/test/java/com/mantzavelas/petclinicspringapp/PetClinicSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantzavelas.petclinicspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
