58254bf54f47a4233854c553b41bd7df2e2e8876
kostasmantz <kostiksm@gmail.com>
2020-11-25 15:54:55 +0200

    - Introduce BaseEntity adds basic id property - Minor change in pom.xml
    Closes #10

--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
@@ -2,7 +2,7 @@ package com.mantzavelas.petclinicspringapp.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/PetType.java
@@ -1,6 +1,6 @@
 package com.mantzavelas.petclinicspringapp.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
