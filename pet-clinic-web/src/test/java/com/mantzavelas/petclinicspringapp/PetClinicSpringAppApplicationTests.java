00b552942bdd4c9c84331acce02ff48fd8bee98f
Kostas Mantzavelas <kostiksm@gmail.com>
2020-12-15 19:33:35 +0200

    Added index page and controller Closes #8

--- pet-clinic-web/src/test/java/com/mantzavelas/petclinicspringapp/PetClinicSpringAppApplicationTests.java
+++ pet-clinic-web/src/test/java/com/mantzavelas/petclinicspringapp/PetClinicSpringAppApplicationTests.java
@@ -1,6 +1,5 @@
-package petclinicspringapp;
+package com.mantzavelas.petclinicspringapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
