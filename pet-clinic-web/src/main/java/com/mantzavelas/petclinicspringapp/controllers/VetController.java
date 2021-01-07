21704eefe6bdfdb8bd52fa65d97f8ec47f573b7c
Kostas Mantzavelas <kostiksm@gmail.com>
2021-01-07 18:24:57 +0200

    Display all the vets in the Vet Index Page
    Closes #14

--- pet-clinic-web/src/main/java/com/mantzavelas/petclinicspringapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mantzavelas/petclinicspringapp/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.mantzavelas.petclinicspringapp.controllers;
 
+import com.mantzavelas.petclinicspringapp.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
