346ca83a4aa6d949842e620350f2ae8f950b55df
Kostas Mantzavelas <kostiksm@gmail.com>
2021-01-18 21:02:12 +0200

    - Introduced Visit entity - Update Owner relationship to have pets
    Closes #25

--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Owner.java
@@ -1,11 +1,23 @@
 package com.mantzavelas.petclinicspringapp.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 
     public Owner() {
     }
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
