7804fee1bf5a3b380478fa496c18f068f6650a58
Kostas Mantzavelas <kostiksm@gmail.com>
2021-01-20 11:59:50 +0200

    - Implemented PetType Map Service
    Closes #28

---
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.mantzavelas.petclinicspringapp.services;
+
+import com.mantzavelas.petclinicspringapp.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.mantzavelas.petclinicspringapp.services.map;
+
+import com.mantzavelas.petclinicspringapp.model.PetType;
+import com.mantzavelas.petclinicspringapp.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long objectId) {
+        super.deleteById(objectId);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
