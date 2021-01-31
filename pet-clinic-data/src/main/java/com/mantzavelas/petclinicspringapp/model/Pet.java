9bf370b4f17d09df28d347758ed38f7dd3cdba0b
Kostas Mantzavelas <kostiksm@gmail.com>
2021-01-31 13:25:01 +0200

    - Converted Owners and related models to JPA Entities
    Closes #35

--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
@@ -1,17 +1,36 @@
 package com.mantzavelas.petclinicspringapp.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public PetType getPetType() {
         return petType;
     }
-
     public void setPetType(PetType petType) {
         this.petType = petType;
     }
@@ -19,7 +38,6 @@ public class Pet extends BaseEntity{
     public Owner getOwner() {
         return owner;
     }
-
     public void setOwner(Owner owner) {
         this.owner = owner;
     }
@@ -27,7 +45,6 @@ public class Pet extends BaseEntity{
     public LocalDate getBirthDate() {
         return birthDate;
     }
-
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/PetType.java
@@ -1,13 +1,19 @@
 package com.mantzavelas.petclinicspringapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
