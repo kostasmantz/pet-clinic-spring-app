cd7e761ff69870a16562c7ba53bd9d17fef65b87
Kostas Mantzavelas <kostiksm@gmail.com>
2021-02-23 12:46:56 +0200

    -Introduce functionality to add/update visits
    #56

--- pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mantzavelas/petclinicspringapp/model/Pet.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Setter
@@ -32,4 +33,10 @@ public class Pet extends BaseEntity{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits;
 
+    public void addVisit(Visit visit) {
+        if (this.visits == null) {
+            this.visits = new HashSet<>();
+        }
+        this.visits.add(visit);
+    }
 }
---
+++ pet-clinic-web/src/main/java/com/mantzavelas/petclinicspringapp/controllers/VisitController.java
@@ -0,0 +1,59 @@
+package com.mantzavelas.petclinicspringapp.controllers;
+
+import com.mantzavelas.petclinicspringapp.model.Pet;
+import com.mantzavelas.petclinicspringapp.model.Visit;
+import com.mantzavelas.petclinicspringapp.services.PetService;
+import com.mantzavelas.petclinicspringapp.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    public static final String VIEWS_PETS_CREATE_OR_UPDATE_VISIT_FORM = "pets/visitForm";
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+
+        Visit visit = new Visit();
+        visit.setPet(pet);
+        pet.addVisit(visit);
+
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable int petId, Model model) {
+        return VIEWS_PETS_CREATE_OR_UPDATE_VISIT_FORM;
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_PETS_CREATE_OR_UPDATE_VISIT_FORM;
+        }
+        visitService.save(visit);
+
+        return "redirect:/owners/{ownerId}";
+    }
+
+}
