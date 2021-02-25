a4c0f5bbc780b11545d769187abc109cfaff76b4
Kostas Mantzavelas <kostiksm@gmail.com>
2021-02-25 21:43:25 +0200

    -Implemented GET /api/vets endpoint Closes#63

--- pet-clinic-web/src/main/java/com/mantzavelas/petclinicspringapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mantzavelas/petclinicspringapp/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.mantzavelas.petclinicspringapp.controllers;
 
+import com.mantzavelas.petclinicspringapp.model.Vet;
 import com.mantzavelas.petclinicspringapp.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +24,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getAllVets() {
+        return vetService.findAll();
     }
 }
